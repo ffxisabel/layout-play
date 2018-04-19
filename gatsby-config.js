a00041092139bc78860a632da4b393692bf8f90a
Isabel Brison <isabel.brison@fairfaxmedia.com.au>
2018-04-19 17:11:40 +1000

    fixed article grid

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass'
-  ],
+  ], 
+  pathPrefix: "/layout-play",
+
 }
--- src/pages/article.js
+++ src/pages/article.js
@@ -8,8 +8,7 @@ const SecondPage = () => (
       <h1>Fun with article layouts</h1>
       <p>American shorthair tiger scottish fold turkish angora</p>
     </header>
-    <section className={styles.byline}><strong>Author Name</strong><p>Author occupation</p></section>
-    <section className={styles.points}><p>Points</p><p>Points</p></section>    
+    <section className={styles.byline}><strong>Author Name</strong><p>Author occupation</p></section>  
     <section className={styles.sectionOne}>
       <p>Cat ipsum dolor sit amet, tabby savannah. Bombay cheetah puma sphynx abyssinian . Persian havana brown or tom russian blue singapura yet thai. Cornish rex ocicat or burmese. British shorthair mouser. Persian balinese yet donskoy birman singapura. Egyptian mau. American shorthair savannah yet balinese so kitten tom mouser puma. Grimalkin tomcat yet ocicat persian donskoy.</p>
       <p>Bombay siamese so singapura. Leopard munchkin, russian blue bobcat. Cornish rex cougar, donskoy for tiger. Scottish fold singapura jaguar havana brown kitten and tiger. British shorthair cougar, panther so bombay. Ragdoll abyssinian for tom. Sphynx burmese ocelot so thai.</p>
